050ba51b9b78eb237403dbaf198ffeb19aa36b2e
Emily Asch <e.asch@me.com>
2018-11-20 20:52:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  
+  def dog_name(name)
+    @name = name 
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+def dog_breed(breed)
+  @breed = breed 
+end
+
+def breed 
+  @breed 
+end
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+  
+end
