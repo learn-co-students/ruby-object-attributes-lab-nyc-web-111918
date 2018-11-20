81adb9fe56603f7992fdfcb2dcb723d6fa2b6b10
Matt Doering <ablueplatypus21@gmail.com>
2018-11-20 16:22:42 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+ def name
+   @dogs_name
+ end
+
+ def name=(new_name)
+    @dogs_name = new_name
+ end
+
+def breed
+ @breed_type
+end
+
+def breed=(breed_name)
+  @breed_type = breed_name
+end
+
+end #end of class method
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name
+    @name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def jobs
+    @jobs
+  end
+
+  def jobs=(persons_job)
+    @jobs = persons_job
+  end
+
+end #end of class method
